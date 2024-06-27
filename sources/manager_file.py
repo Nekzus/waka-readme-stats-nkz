fac50e5e28209ca52acf5f9bca712588ef9bae65
Nekzus <85507589+Nekzus@users.noreply.github.com>
2024-06-26 23:44:05 -0300

    bar_graph

--- sources/manager_file.py
+++ sources/manager_file.py
@@ -1,7 +1,6 @@
-from os.path import join, isfile, dirname
-from pickle import load as load_pickle, dump as dump_pickle
-from json import load as load_json
-from typing import Dict, Optional, Any
+from json import load
+from os.path import join, dirname
+from typing import Dict
 
 from manager_environment import EnvironmentManager as EM
 
@@ -20,7 +19,6 @@ class FileManager:
     Stores localization in dictionary.
     """
 
-    ASSETS_DIR = "assets"
     _LOCALIZATION: Dict[str, str] = dict()
 
     @staticmethod
@@ -31,7 +29,7 @@ class FileManager:
         :param file: Localization file path, related to current file (in sources root).
         """
         with open(join(dirname(__file__), file), encoding="utf-8") as config_file:
-            data = load_json(config_file)
+            data = load(config_file)
         FileManager._LOCALIZATION = data[EM.LOCALE]
 
     @staticmethod
@@ -54,30 +52,6 @@ class FileManager:
         :param append: True for appending to file, false for rewriting.
         :param assets: True for saving to 'assets' directory, false otherwise.
         """
-        name = join(FileManager.ASSETS_DIR, name) if assets else name
+        name = join("assets", name) if assets else name
         with open(name, "a" if append else "w", encoding="utf-8") as file:
             file.write(content)
-
-    @staticmethod
-    def cache_binary(name: str, content: Optional[Any] = None, assets: bool = False) -> Optional[Any]:
-        """
-        Save binary output file if provided or read if content is None.
-
-        :param name: File name.
-        :param content: File content (utf-8 string) or None.
-        :param assets: True for saving to 'assets' directory, false otherwise.
-        :returns: File cache contents if content is None, None otherwise.
-        """
-        name = join(FileManager.ASSETS_DIR, name) if assets else name
-        if content is None and not isfile(name):
-            return None
-
-        with open(name, "rb" if content is None else "wb") as file:
-            if content is None:
-                try:
-                    return load_pickle(file)
-                except Exception:
-                    return None
-            else:
-                dump_pickle(content, file)
-                return None
