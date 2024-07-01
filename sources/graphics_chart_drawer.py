c28eb7f4183cf0bb3316dc9d80865a2e36ee8b9c
Nekzus <85507589+Nekzus@users.noreply.github.com>
2024-06-30 22:26:31 -0300

    loc added

--- sources/graphics_chart_drawer.py
+++ sources/graphics_chart_drawer.py
@@ -1,16 +1,21 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from typing import Dict
+
+from numpy import arange, array, add, amax, zeros
 import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
 from manager_download import DownloadManager as DM
+
 
 MAX_LANGUAGES = 5  # Number of top languages to add to chart, for each year quarter
 GRAPH_PATH = "assets/bar_graph.png"  # Chart saving path.
 DEFAULT_COLOR = "#808080"  # Gray color as default
 
-async def create_loc_graph(yearly_data: dict, save_path: str):
+
+async def create_loc_graph(yearly_data: Dict, save_path: str):
     """
-    Draws graph of lines of code added by user by quarters of years.
-    Picks top MAX_LANGUAGES languages from each quarter only.
+    Draws graph of lines of code written by user by quarters of years.
+    Picks top `MAX_LANGUAGES` languages from each quarter only.
 
     :param yearly_data: GitHub user yearly data.
     :param save_path: Path to save the graph file.
@@ -18,35 +23,36 @@ async def create_loc_graph(yearly_data: dict, save_path: str):
     colors = await DM.get_remote_yaml("linguist")
 
     years = len(yearly_data.keys())
-    year_indexes = np.arange(years)
+    year_indexes = arange(years)
 
-    languages_all_loc = {}
+    languages_all_loc = dict()
     for i, y in enumerate(sorted(yearly_data.keys())):
         for q in yearly_data[y].keys():
             langs = sorted(yearly_data[y][q].keys(), key=lambda n: yearly_data[y][q][n], reverse=True)[0:MAX_LANGUAGES]
 
             for lang in langs:
                 if lang not in languages_all_loc:
-                    languages_all_loc[lang] = np.zeros((4, years), dtype=int)
-                languages_all_loc[lang][q - 1, i] = yearly_data[y][q][lang]
+                    languages_all_loc[lang] = zeros((4, years), dtype=int)
+                added_lines = yearly_data[y][q][lang].get("added", 0)
+                deleted_lines = yearly_data[y][q][lang].get("deleted", 0)
+                languages_all_loc[lang][q - 1][i] = max(0, added_lines - deleted_lines)
 
     fig = plt.figure()
     ax = fig.add_axes([0, 0, 1.5, 1])
 
     language_handles = []
-    cumulative = np.zeros((4, years), dtype=int)
+    cumulative = zeros((4, years), dtype=int)
 
     for key, value in languages_all_loc.items():
         color = colors.get(key, {}).get("color", DEFAULT_COLOR)
         language_handles += [mpatches.Patch(color=color, label=key)]
 
         for quarter in range(4):
-            loc_added = np.maximum(value[quarter] - cumulative[quarter], 0)
-            ax.bar(year_indexes + quarter * 0.21, loc_added, 0.2, bottom=cumulative[quarter], color=color)
-            cumulative[quarter] += value[quarter]
+            ax.bar(year_indexes + quarter * 0.21, value[quarter], 0.2, bottom=cumulative[quarter], color=color)
+            cumulative[quarter] = add(cumulative[quarter], value[quarter])
 
     ax.set_ylabel("LOC added", fontdict=dict(weight="bold"))
-    ax.set_xticks(np.array([np.arange(i, i + 0.84, step=0.21) for i in year_indexes]).flatten(), labels=["Q1", "Q2", "Q3", "Q4"] * years)
+    ax.set_xticks(array([arange(i, i + 0.84, step=0.21) for i in year_indexes]).flatten(), labels=["Q1", "Q2", "Q3", "Q4"] * years)
 
     sax = ax.secondary_xaxis("top")
     sax.set_xticks(year_indexes + 0.42, labels=sorted(yearly_data.keys()))
@@ -59,6 +65,6 @@ async def create_loc_graph(yearly_data: dict, save_path: str):
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
 
-    plt.ylim(0, 1.05 * np.amax(cumulative))
+    plt.ylim(0, 1.05 * amax(cumulative))
     plt.savefig(save_path, bbox_inches="tight")
     plt.close(fig)
